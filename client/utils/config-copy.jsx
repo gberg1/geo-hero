99e972f8010f1c221ae905ed312527de77d20a79
David Goldberg <drgold1988@gmail.com>
2015-09-04 11:34:26 -0700

    [feat]: adds file containing config keys and sensitive info

---
+++ client/utils/config-copy.jsx
@@ -0,0 +1,15 @@
+var GoogleMapsLoader = require('google-maps');
+
+var keys = {
+    googleMaps: {
+      GoogleMapsLoader.KEY = 'qwertyuiopasdfghjklzxcvbnm',
+      GoogleMapsLoader.CLIENT = 'INSERT_CLIENT_KEY',
+      GoogleMapsLoader.VERSION = '3.14',
+      GoogleMapsLoader.SENSOR = true,
+      GoogleMapsLoader.LIBRARIES = ['geometry', 'places'],
+      GoogleMapsLoader.LANGUAGE = 'en'
+    }
+  }
+};
+
+module.exports = keys;
